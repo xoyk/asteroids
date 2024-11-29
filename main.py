0fccdc4ceb5e68fc5f4f60570ca1f42092a5297d
xoyk <xoyk82@gmail.com>
2024-11-29 09:42:47 +0400

    add starting screen

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def main():
     print("Starting asteroids!")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
