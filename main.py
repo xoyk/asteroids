b9b9ca55d330dd434657ecab1f28acda37b44fd8
xoyk <xoyk82@gmail.com>
2024-12-01 01:26:41 +0400

    add drawable and updatable groups for objects

--- main.py
+++ main.py
@@ -3,24 +3,34 @@ from constants import *
 from player import Player
 
 def main():
-    print("Starting asteroids!")
-    print("Screen width:",SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    
+    
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
         
-        player.draw(screen)
-        dt = clock.tick(60) / 1000
-        player.update(dt)
+        for updatable_item in updatable:
+            updatable_item.update(dt)
+        
+        screen.fill((0,0,0))
+
+        for drawable_item in drawable:
+            drawable_item.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         
 
 
