4205f959bf74491e3d004daf6d722be832016767
xoyk <xoyk82@gmail.com>
2024-12-01 01:17:20 +0400

    add ship moving

--- player.py
+++ player.py
@@ -27,5 +27,16 @@ class Player (CircleShape):
 
         if keys[pygame.K_a]:
             self.rotate(dt)
+        
         if keys[pygame.K_d]:
             self.rotate(dt * -1)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
